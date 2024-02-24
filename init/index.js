5a2b5248ac0579a414d127afe47c7b0e174c40b3
manikerisaurabh <manikerisaurabh@gmail.com>
2024-02-24 07:21:06 +0530

    add some stylling

--- init/index.js
+++ init/index.js
@@ -3,7 +3,7 @@ const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/journeynest";
-
+const Db_url = `${process.env.ATLASDB_URL}`;
 main()
     .then(() => {
         console.log("connected to DB");
