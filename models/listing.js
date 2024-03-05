404033282346be87f3ab79c3eaf39bc4d059780e
manikerisaurabh <manikerisaurabh@gmail.com>
2024-03-06 00:47:09 +0530

    map updated

--- models/listing.js
+++ models/listing.js
@@ -30,11 +30,9 @@ const listingSchema = new Schema({
         type: {
             type: String, // Don't do `{ location: { type: String } }`
             enum: ['Point'], // 'location.type' must be 'Point'
-            required: true
         },
         coordinates: {
-            type: [Number],
-            required: true
+            type: [Number]
         }
     }
 });
